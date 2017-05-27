00449fc943f76dd023992768d9ee2c1090bbbf47
yangran <sfyr111@163.com>
2017-05-27 23:39:16 +0800

    封装getPostData函数，编写getPostData回调，获取注册信息 email, password, password_confirmation

--- login/server.js
+++ login/server.js
@@ -1,19 +1,26 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
 
-var port = process.env.PORT || 8000
+const port = process.env.PORT || 8000
 
-var server = http.createServer(function(request, response) {
-  var temp = url.parse(request.url, true)
-  var path = temp.pathname
-  var query = temp.query
-  var method = request.method
+const server = http.createServer((request, response) => {
+  let temp = url.parse(request.url, true)
+  let path = temp.pathname
+  let query = temp.query
+  let method = request.method
 
   if(path === '/') {
     var string = fs.readFileSync('./index.html')
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.end(string)
+  } else if(path === '/signUp' && method === 'POST') {
+    getPostData(request, postData => {
+      let email = postData.email
+      let password = postData.password
+      let password_confirmation = postData.password_confirmation
+      response.end(JSON.stringify(postData))
+    })
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
@@ -21,5 +28,25 @@ var server = http.createServer(function(request, response) {
   }
   console.log(method + ' ' + request.url)  
 })
+
+function getPostData(request, callback) {
+  data = ''
+  request.on('data', chunk => {
+    data += chunk.toString()
+  })
+
+  request.on('end', () => {
+    let array = data.split('&')
+    let postData = {}
+    array.forEach(kv => {
+      let parts = kv.split('=')
+      let key = parts[0]
+      let value = parts[1]
+      postData[key] = value
+    })
+    callback.call(null, postData)
+  })
+}
+
 server.listen(port)
 console.log('监听 ' + port + ' 成功, 打开http://localhost:' + port) 
