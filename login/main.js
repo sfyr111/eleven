f9fcccf894fe58f745ac0b80c921589d4ee7bfad
yangran <sfyr111@163.com>
2017-05-28 02:28:22 +0800

    逻辑基本完成，cookie等

--- login/main.js
+++ login/main.js
@@ -1,39 +1,69 @@
+let $loginForm = $('form[name=login]')
+$loginForm.on('submit', (e) => {
+  e.preventDefault()
+  let string = $loginForm.serialize()
+  $.ajax({
+    url: $loginForm.attr('action'),
+    method: $loginForm.attr('method'),
+    data: string,
+    success() {
+      location.href = '/home'
+    },
+    error() {
+
+    }
+  })
+})
+
 let $signUpForm = $('form[name=signUp]')
 $signUpForm.on('submit', (e) => {
   e.preventDefault()
   let string = $signUpForm.serialize() // 表单序列化
   // check form
+  let errors = checkForm($signUpForm)
+
+  if (Object.keys(errors).length !== 0) {
+    showErrors($signUpForm, errors)
+  } else {
+    $.ajax({
+      url: $signUpForm.attr('action'),
+      method: $signUpForm.attr('method'),
+      data: string,
+      success(response) {
+        alert('注册成功')
+      },
+      error(err) {
+        let errors = JSON.parse(err.responseText)
+        showErrors($signUpForm, errors)
+      }
+    })
+  }
+})
+
+function checkForm($signUpForm) {
   let email = $signUpForm.find('[name=email]').val()
   let password = $signUpForm.find('[name=password]').val()
   let password_confirmation = $signUpForm.find('[name=password_confirmation]').val()
 
   let errors = {}
-  if(email.indexOf('@') < 0) {
+  if (email.indexOf('@') < 0) {
     errors.email = '邮箱不合法'
   }
-  if(password.length < 6) {
+  if (password.length < 6) {
     errors.password = '密码太短'
   }
-  if(password_confirmation !== password) {
+  if (password_confirmation !== password) {
     errors.password_confirmation = '两次密码输入不匹配'
   }
-  if(Object.keys(errors).length !== 0) {
-    $signUpForm.find(`span[name$=_error]`).each((index, el) => {
-      $(el).text('')
-    })
-    for(let key in errors) {
-      let value = errors[key]
-      $signUpForm.find(`span[name=${key}_error]`).text(value)
-    }
-    return
-  }
-  $.ajax({
-    url: $signUpForm.attr('action'),
-    method: $signUpForm.attr('method'),
-    data: string,
-    success(response) {
-      let object = JSON.parse(response)
-      console.log(object)
-    }
+  return errors
+}
+
+function showErrors($signUpForm, errors) {
+  $signUpForm.find(`span[name$=_error]`).each((index, el) => {
+    $(el).text('')
   })
-})
+  for (let key in errors) {
+    let value = errors[key]
+    $signUpForm.find(`span[name=${key}_error]`).text(value)
+  }
+}
--- login/server.js
+++ login/server.js
@@ -10,41 +10,80 @@ const server = http.createServer((request, response) => {
   let query = temp.query
   let method = request.method
 
-  if(path === '/') {
+  if (path === '/') {
     var string = fs.readFileSync('./index.html')
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.end(string)
-  } else if(path === '/signUp' && method === 'POST') {
+  } else if (path === '/signUp' && method === 'POST') {
     getPostData(request, postData => {
-      let { email, password, password_confirmation } = postData
-      let errors = {}
-      // check email
-      if(email.indexOf('@') < 0) {
-        errors.email = '邮箱不合法'
+      let errors = checkPostData(postData)
+      if (Object.keys(errors).length === 0) {
+        let { email, password } = postData
+        let user = {
+          email,
+          passwordHash: hash(password) // 加密算法
+        }
+        // 写数据库
+        let dbString = fs.readFileSync('./db.json', 'utf-8')
+        let dbObject = JSON.parse(dbString)
+        dbObject.users.push(user)
+        let dbString2 = JSON.stringify(dbObject)
+        fs.writeFileSync('./db.json', dbString2, { encoding: 'utf-8' })
+      } else {
+        response.statusCode = 400
       }
-      if(password.length < 6) {
-        errors.password = '密码太短'
-      }
-      if(password_confirmation !== password) {
-        errors.password_confirmation = '两次密码输入不匹配'
-      }
+
       response.setHeader('Content-Type', 'text/html;charset=utf-8')
       response.end(JSON.stringify(errors))
     })
-  } else if(path === '/node_modules/jquery/dist/jquery.min.js') {
+  } else if (path === '/node_modules/jquery/dist/jquery.min.js') {
     let string = fs.readFileSync('./node_modules/jquery/dist/jquery.min.js')
     response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
     response.end(string)
-  } else if(path === '/main.js') {
+  } else if (path === '/main.js') {
     let string = fs.readFileSync('./main.js')
     response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
     response.end(string)
+  } else if (path === '/home') {
+    let cookies = parseCookies(request.headers.cookie)
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    if(cookies.logined === 'true') {
+      response.end(`${cookies.user_id}已登录`)
+    } else {
+      let string = fs.readFileSync('./home.html')
+      response.end(string)
+    }
+  } else if (path === '/login' && method === 'POST') {
+    // 读数据库
+    getPostData(request, (postData) => {
+      let dbString = fs.readFileSync('./db.json', 'utf-8')
+      let dbObject = JSON.parse(dbString)
+      let users = dbObject.users
+      let { email, password } = postData
+      let found
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email && users[i].passwordHash === hash(password)) {
+          found = users[i]
+          break
+        }
+      }
+      if (found) {
+        // 标记用户登录cookie
+        response.setHeader('Set-Cookie', ['logined=true; expires=1000; path=/;', 'user_id=' + email + '; expires=123456789; path=/;'])
+        response.end('')
+      } else {
+        response.statusCode = 400
+        let errors = {email: '没注册或密码错误'} // 返回错误没处理
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.end(JSON.stringify(errors))
+      }
+    })
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.end('找不到对应的路径， 你需要自行修改 index.js')
   }
-  console.log(method + ' ' + request.url)  
+  console.log(method + ' ' + request.url)
 })
 
 function getPostData(request, callback) {
@@ -64,6 +103,51 @@ function getPostData(request, callback) {
     })
     callback.call(null, postData)
   })
+}
+
+function checkPostData(postData) {
+  let { email, password, password_confirmation } = postData
+  let errors = {}
+  // check email
+  if (email.indexOf('@') < 0) {
+    errors.email = '邮箱不合法'
+  }
+  if (password.length < 6) {
+    errors.password = '密码太短'
+  }
+  if (password_confirmation !== password) {
+    errors.password_confirmation = '两次密码输入不匹配'
+  }
+  return errors
+}
+
+function hash(string) {
+  return string + 'hash'
+}
+
+function parseCookies(cookie) { // JSON.parse
+  try {
+    return cookie.split(';').reduce(
+      function (prev, curr) {
+        var m = / *([^=]+)=(.*)/.exec(curr);
+        var key = m[1];
+        var value = decodeURIComponent(m[2]);
+        prev[key] = value;
+        return prev;
+      },
+      {}
+    );
+  } catch (error) {
+    return {}
+  }
+}
+
+function stringifyCookies(cookies) { //JSON.stringify
+  var list = [];
+  for (var key in cookies) {
+    list.push(key + '=' + encodeURIComponent(cookies[key]));
+  }
+  return list.join('; ');
 }
 
 server.listen(port)
